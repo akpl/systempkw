adc274bb22c023037a40fc843375512f844c5ccf
Elimas <Elimas@users.noreply.github.com>
2016-06-10 15:43:43 +0200

    autoryzacja wysyłania protokołu z wyborów

---
+++ src/main/java/pkw/models/AutoryzacjaHaslem.java
@@ -0,0 +1,17 @@
+package pkw.models;
+
+/**
+ * Created by Elimas on 2016-06-10.
+ */
+public class AutoryzacjaHaslem {
+
+    private String password;
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
